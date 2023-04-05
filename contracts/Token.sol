97aff762d7c0d637887b45d63719aa03c00d7462
ArielRobotti <arielrobotti@gmail.com>
2023-04-04 22:22:14 -0300

    Clase2.5

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// address 0x3c1e48D47fd2FC171a02B1257624469d943d29E8
 pragma solidity 0.5.2;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol';
@@ -6,5 +7,7 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 contract Token is ERC20Mintable{
         string public name = "PiToken";
         string public symbol = "314";
-        uint8 public decimals = 2; 
+        uint8 public decimals = 2;
+
+        
 }
